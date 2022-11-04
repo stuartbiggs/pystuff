a277f9cbe125d04cc14b5b600e61edef2262e367
Stuart Biggs <stuartbiggs@Stuarts-iMac.lan>
2022-11-04 11:27:56 +0000

    program updated

--- tapp.py
+++ tapp.py
@@ -4,9 +4,18 @@ def getName():
     name = input("What is your name: ")
     return name
 
-def displayName():
+def getAge():
+    age = input("Please enter your age:")
+    return age
+
+
+
+def displayInfo():
     name = getName()
+    age = getAge()
     print("hello " + name)
+    print("Your age is :" + age)
+
 
 #getName()
-displayName()
+displayInfo()
